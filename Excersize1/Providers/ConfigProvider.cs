ba24a2183b3cd82d27d55faea33b8cc718616627
Ramya Thondapu <Ramya_Thondapu@epam.com>
2024-11-04 23:48:11 +0530

    Base application

---
+++ Excersize1/Providers/ConfigProvider.cs
@@ -0,0 +1,19 @@
+﻿using core.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersize1.Providers
+{
+    public class ConfigProvider : ConfigurationProvider
+
+    {
+        static string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../,../,../", "config.json");
+        public static Model.ApplicationDetails apDetails => Load<Model.ApplicationDetails>(path,"ApplicationDetails");
+
+        public static core.Models.BrowserConfiguration browseConfiguration => Load<core.Models.BrowserConfiguration>(path, "Environment");
+
+    }
+}
